1edb80bdcc55b70d85d8857225f9dd2ef0c5b714
ibjyot <igill47@my.bcit.ca>
2023-02-07 19:52:02 -0800

    side list integrated along with animations for nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
-import 'animations.dart';                               // Add this import
+import 'animations.dart';  
+import 'transitions/list_detail_transition.dart'; 
 import 'models/data.dart' as data;
 import 'models/models.dart';
 import 'widgets/animated_floating_action_button.dart';  // Add this import
 import 'widgets/disappearing_bottom_navigation_bar.dart';
 import 'widgets/disappearing_navigation_rail.dart';
 import 'widgets/email_list_view.dart';
+import 'widgets/reply_list_view.dart'; 
 
 void main() {
   runApp(const MainApp());
@@ -113,14 +115,18 @@ class _FeedState extends State<Feed> with SingleTickerProviderStateMixin {
               Expanded(
                 child: Container(
                   color: _backgroundColor,
-                  child: EmailListView(
-                    selectedIndex: selectedIndex,
-                    onSelected: (index) {
-                      setState(() {
-                        selectedIndex = index;
-                      });
-                    },
-                    currentUser: widget.currentUser,
+                  child: ListDetailTransition(
+                    animation: _railAnimation,
+                    one: EmailListView(
+                      selectedIndex: selectedIndex,
+                      onSelected: (index) {
+                        setState(() {
+                          selectedIndex = index;
+                        });
+                      },
+                      currentUser: widget.currentUser,
+                    ),
+                    two: const ReplyListView(),
                   ),
                 ),
               ),
